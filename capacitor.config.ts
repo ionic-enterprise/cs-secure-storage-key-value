bf763f9928a30eb3c0880b892469f9e1c3824954
Damian Tarnawsky <damian@ionic.io>
2022-06-10 14:30:39 -0700

    Update theme for Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,8 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'star-wars',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  backgroundColor: 'black'
 };
 
 export default config;
